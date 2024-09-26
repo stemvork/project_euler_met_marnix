69904412563a604a2e3be4c96bd94fbb7f903386
een <een@emmauscollege.nl>
2024-09-26 14:10:02 +0200

    Solved day 7.

--- 7.py
+++ 7.py
@@ -1,29 +1,22 @@
 import math
-import numpy as np
-from functools import lru_cache
-
-def sieve(n):
+def zeef_van_eratosthenes(n): # genereert priemgetallen tot en met n
+    n = int(n)
     if n < 2:
         return []
-    
+
+    # Maak een lijst met booleans en initialiseer alle waarden op True
     primes = [True] * (n + 1)
-    primes[0] = primes[1] = False # 0 en 1 zijn geen priemgetallen
-    
+    primes[0] = primes[1] = False  # 0 en 1 zijn geen priemgetallen
+
     for i in range(2, int(math.sqrt(n)) + 1):
         if primes[i]:
+            # Zet alle veelvouden van i op False
             for j in range(i*i, n + 1, i):
                 primes[j] = False
+
+    # Genereer de lijst van priemgetallen
     return [i for i, prime in enumerate(primes) if prime]
 
-def next_prime(above, primes):
-    if(primes[-1] >= above): 
-        return False
-    
-@lru_cache(maxsize=None)
-def factorial(n):
-    return n * factorial(n-1) if n else 1
-
-print(factorial(10))
-print(factorial(5))
-print(factorial(12))
+primes = zeef_van_eratosthenes(2000000)
+print(primes[10000])
 
